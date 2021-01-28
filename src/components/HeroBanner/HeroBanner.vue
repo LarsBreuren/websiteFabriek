e102e674528ec840ec1cc3938023969bd15c677c
zack siri <larsbreuren@gmail.com>
2021-01-28 20:33:53 +0100

    Hero banner now responsive af

--- src/components/HeroBanner/HeroBanner.vue
+++ src/components/HeroBanner/HeroBanner.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex justify-around items-center bg-white wrapper">
-        <div class="w-1/3 md:flex justify-center first">
-            <div class="w-2/3 bg-secondary-500 vertical-pill "></div>
+    <div class="w-3/4 p-16 sm:p-0 sm:w-full mx-auto flex justify-between items-center bg-white wrapper">
+        <div class="w-1/3 lg:flex justify-center pill1">
+            <div class=" bg-secondary-500 vertical-pill "></div>
         </div>
-        <div class="w-1/2 md:w-1/3 flex flex-wrap justify-center">
+        <div class=" lg:w-1/3 flex flex-wrap justify-center flex-grow">
             <h1 class="text-center font-bold text-4xl md:text-6xl">Simpel.<br>
                 Effectief.<br>
                 Voordelig.
@@ -13,8 +13,8 @@
             <div class="ctaButton mt-16">Contact</div>
         </router-link>
         </div>
-        <div class="w-1/2 md:w-1/3 flex justify-center ">
-            <div class="w-full md:w-2/3 bg-secondary-500 darth vertical-pill"></div>
+        <div class=" lg:w-1/3 flex justify-end lg:justify-center">
+            <div class=" bg-secondary-500  pill2 vertical-pill"></div>
         </div>
 
 
