7aca41b79e54dd10f42cb1ba553e8e62cf99c8a7
zack siri <larsbreuren@gmail.com>
2021-01-28 22:24:29 +0100

    Hero banner responsive design upgrade

--- src/components/HeroBanner/HeroBanner.vue
+++ src/components/HeroBanner/HeroBanner.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="w-3/4 p-16 sm:p-0 sm:w-full mx-auto flex justify-between items-center bg-white wrapper">
+    <div class=" p-16 sm:p-0 w-full mx-auto flex justify-between items-center bg-white wrapper">
+        <div class="overlay"></div>
         <div class="w-1/3 lg:flex justify-center pill1">
             <div class=" bg-secondary-500 vertical-pill "></div>
         </div>
         <div class=" lg:w-1/3 flex flex-wrap justify-center flex-grow">
-            <h1 class="text-center font-bold text-header text-4xl md:text-6xl">Simpel.<br>
+            <h1 class="text-center font-bold text-white sm:text-header text-4xl md:text-6xl">Simpel.<br>
                 Effectief.<br>
                 Voordelig.
             </h1>
