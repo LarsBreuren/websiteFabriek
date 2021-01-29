7867baed59d0c208e504dd6847ba8670c0ec3503
zack siri <larsbreuren@gmail.com>
2021-01-29 11:45:05 +0100

    New webdesigns added

--- src/components/OurWork/OurWork.vue
+++ src/components/OurWork/OurWork.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="container">
+  <div class="container my-32 md:my-128">
     <div class="flex flex-wrap justify-between">
-        <div class="w-full md:text-left"><h2 class="text-4xl my-16">Onze recente webdesigns</h2></div>
+        <div class="w-full md:text-left"><h2 class="text-4xl mb-16">Onze recente webdesigns</h2></div>
         <div class="flex flex-wrap">
-          <div class="w-full sm:w-1/2 lg:w-1/3 px-16 design">
-            <img class="w-full mx-auto" alt="One of our webdesigns" src="https://www.premiumswebsites.nl/userfiles/premiumswebsites.nl/images/Mocku_Karakter_Vloeren.png">
+          <div class="w-full sm:w-1/2 px-32 lg:w-1/3 px-16 design relative my-32">
+            <img class="w-4/5 mx-auto" alt="One of our webdesigns" src="../../assets/images/portfolio/praktijk-desktop.png">
+            <img class="w-1/5 mx-auto absolute mobile" alt="One of our webdesigns" src="../../assets/images/portfolio/praktijk-mobile.png">
           </div>
-          <div class="w-full sm:w-1/2 lg:w-1/3 px-16 design">
-            <img class="w-full mx-auto" alt="One of our webdesigns" src="https://www.premiumswebsites.nl/userfiles/premiumswebsites.nl/images/Mockup_Urban_Beauty_bar.png">
+          <div class="w-full sm:w-1/2 px-32 lg:w-1/3 px-16 design relative my-32">
+            <img class="w-4/5 mx-auto" alt="One of our webdesigns" src="../../assets/images/portfolio/benb-desktop.png">
+            <img class="w-1/5 mx-auto absolute mobile" alt="One of our webdesigns" src="../../assets/images/portfolio/benb-mobile.png">
           </div>
-          <div class="w-full sm:w-1/2 lg:w-1/3 px-16 design">
-            <img class="w-full mx-auto" alt="One of our webdesigns" src="https://www.premiumswebsites.nl/userfiles/premiumswebsites.nl/images/Mocku_Karakter_Vloeren.png">
+          <div class="w-full sm:w-1/2 px-32 lg:w-1/3 px-16 design relative my-32">
+            <img class="w-4/5 mx-auto" alt="One of our webdesigns" src="../../assets/images/portfolio/timmer-desktop.png">
+            <img class="w-1/5 mx-auto absolute mobile" alt="One of our webdesigns" src="../../assets/images/portfolio/timmer-mobile.png">
           </div>
-          <div class="w-full sm:w-1/2 lg:w-1/3 px-16 design">
-            <img class="w-full mx-auto" alt="One of our webdesigns" src="https://www.premiumswebsites.nl/userfiles/premiumswebsites.nl/images/Mockup_Urban_Beauty_bar.png">
+          <div class="w-full sm:w-1/2 px-32 lg:w-1/3 px-16 design relative my-32">
+            <img class="w-4/5 mx-auto" alt="One of our webdesigns" src="../../assets/images/portfolio/benb-desktop.png">
+            <img class="w-1/5 mx-auto absolute mobile" alt="One of our webdesigns" src="../../assets/images/portfolio/benb-mobile.png">
           </div>
-          <div class="w-full sm:w-1/2 lg:w-1/3 px-16 design">
-            <img class="w-full mx-auto" alt="One of our webdesigns" src="https://www.premiumswebsites.nl/userfiles/premiumswebsites.nl/images/Mocku_Karakter_Vloeren.png">
+          <div class="w-full sm:w-1/2 px-32 lg:w-1/3 px-16 design relative my-32">
+            <img class="w-4/5 mx-auto" alt="One of our webdesigns" src="../../assets/images/portfolio/timmer-desktop.png">
+            <img class="w-1/5 mx-auto absolute mobile" alt="One of our webdesigns" src="../../assets/images/portfolio/timmer-mobile.png">
           </div>
-          <div class="w-full sm:w-1/2 lg:w-1/3 px-16 design">
-            <img class="w-full mx-auto" alt="One of our webdesigns" src="https://www.premiumswebsites.nl/userfiles/premiumswebsites.nl/images/Mockup_Wolverine_Reizen.png">
+          <div class="w-full sm:w-1/2 px-32 lg:w-1/3 px-16 design relative my-32">
+            <img class="w-4/5 mx-auto" alt="One of our webdesigns" src="../../assets/images/portfolio/praktijk-desktop.png">
+            <img class="w-1/5 mx-auto absolute mobile" alt="One of our webdesigns" src="../../assets/images/portfolio/praktijk-mobile.png">
           </div>
-        </div>
     </div>
-    </div>
+  </div>
+</div>
 </template>
   
  
