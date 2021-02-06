9c46fabae9e807f89347e44a702b27b6974def54
zack siri <larsbreuren@gmail.com>
2021-02-06 19:37:27 +0100

    package header & cleanup

--- src/components/Prices/Prices.vue
+++ src/components/Prices/Prices.vue
@@ -1,90 +1,48 @@
 <template>
   <div class="container md:my-128">
-    <button onclick=hello()>Hi</button>
-    <section class="Accordion">
-      <ul class="Accordion__tabs">
-        <li class="Accordion__tab" onclick="toggleAccordion(this)">
-          <div class="Accordion__tab__headline">
-            <h4>Lorem ipsum dolor sit amet</h4><span class="icon"></span>
-          </div>
-          <div class="Accordion__tab__content">
-            <div class="wrapper">
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-              </p>
-            </div>
-          </div>
-        </li>
-        <li class="Accordion__tab" onclick="toggleAccordion(this)">
-          <div class="Accordion__tab__headline">
-            <h4>dolor sit amet</h4><span class="icon"></span>
-          </div>
-          <div class="Accordion__tab__content">
-            <div class="wrapper">
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
-    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-              </p>
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              </p>
-            </div>
-          </div>
-        </li>
-        <li class="Accordion__tab" onclick="toggleAccordion(this)">
-          <div class="Accordion__tab__headline">
-            <h4>ipsum dolor sit amet</h4><span class="icon"></span>
-          </div>
-          <div class="Accordion__tab__content">
-            <div class="wrapper">
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-              </p>
-            </div>
-          </div>
-        </li>
-        <li class="Accordion__tab" onclick="toggleAccordion(this)">
-          <div class="Accordion__tab__headline">
-            <h4>Lorem ipsum</h4><span class="icon"></span>
-          </div>
-          <div class="Accordion__tab__content">
-            <div class="wrapper">
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-              </p>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </section>
-
     <div class="flex flex-wrap w-full justify-around">
       <div class="mx-32 w-2/5 package">
-        <div class="packageBanner">
-          <h3 class="packageHeader">
+        <div class="packageBanner relative flex justify-center items-center">
+          <h3 class="packageHeader z-20 text-4xl uppercase text-white font-bold">
             Website compleet <br />
-            <span>&euro;699</span>
+            <span class="text-6xl">&euro;699,-</span>
           </h3>
+          <img
+            src="../../assets/images/features/coding.jpeg"
+            class="absolute top-0 bottom-0 right-0 left-0 object-cover w-full h-full"
+          />
+          <div class="overlay absolute top-0 bottom-0 right-0 left-0 object-cover w-full h-full"></div>
           <div class="layer"></div>
         </div>
         <div class="relative label"><p class="text-white">Inclusief:</p></div>
         <ul class="flex text-left">
           <li class="flex-grow">
             <v-icon
-            size="30"
-            class= "
+              size="30"
+              class="
             text-main
             cursor-pointer
             w-full
             inline
           "
-            > mdi-chevron-down</v-icon>
+            >
+              mdi-chevron-down</v-icon
+            >
             Website design
           </li>
-          <li class="w-1/10"> <v-icon
-            size="30"
-            class= "
+          <li class="w-1/10">
+            <v-icon
+              size="30"
+              class="
             text-main
             cursor-pointer
             w-full
             inline
           "
-            > mdi-check</v-icon></li>
+            >
+              mdi-check</v-icon
+            >
+          </li>
         </ul>
       </div>
     </div>
