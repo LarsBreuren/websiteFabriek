985cf8c3edbcb2103016027e85b4eab7919ddf0a
zack siri <larsbreuren@gmail.com>
2021-02-06 22:02:26 +0100

    Prices cards done

--- src/components/Prices/Prices.vue
+++ src/components/Prices/Prices.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="container md:my-128">
     <div class="flex flex-wrap w-full justify-around">
-      <div class="mx-32 w-2/5 package">
+
+      <div class="mx-32 my-32 md:my-0 w-full md:w-2/5 self-start package bg-white">
         <div class="packageBanner relative flex justify-center items-center">
           <h3
             class="packageHeader z-20 text-4xl uppercase text-white font-bold"
           >
             Website compleet <br />
-            <span class="text-6xl">&euro;699,-</span>
+            <span class="text-6xl">&euro;799,-</span>
           </h3>
           <img
             src="../../assets/images/features/coding.jpeg"
@@ -21,8 +22,9 @@
         <div class="relative label py-8 px-32 font-semibold bg-secondary">
           <p class="text-white">Inclusief:</p>
         </div>
+
         <div
-          class="bg-white w-full border border-gray-200 divide-y divide-gray-200"
+          class=" w-full p-8 "
           v-for="(value, name) in websiteComplete"
           :key="value.id"
         >
@@ -32,7 +34,7 @@
               @click="e => e.target.parentElement.classList.toggle('active')"
             ></div>
             <div
-              class=" flex flex-wrap justify-between relative items-center question px-32 py-8"
+              class=" flex flex-wrap justify-between relative items-center question px-32"
             >
             <v-icon
             size="30"
@@ -64,7 +66,88 @@
             </div>
           </div>
         </div>
+        <div class="text-center text-main py-32 ">
+            <p class="font-semibold">25,- Per maand</p>
+            <p class="text-base">Inclusief hosting + domeinnaam</p>
+            <router-link to="/">
+              <div class="ctaButton mt-8 py-8 px-32 inline-block">Bestel</div>
+            </router-link>
+        </div>
       </div>
+
+      <div class="mx-32 my-32 md:my-0 w-full md:w-2/5  self-start package bg-white">
+        <div class="packageBanner relative flex justify-center items-center">
+          <h3
+            class="packageHeader z-20 text-4xl uppercase text-white font-bold"
+          >
+            Maatwerk website <br />
+            <span class="text-6xl">&euro;1299,-</span>
+          </h3>
+          <img
+            src="../../assets/images/features/maatwerk.jpg"
+            class="absolute top-0 bottom-0 right-0 left-0 object-cover w-full h-full"
+          />
+          <div
+            class="overlay absolute top-0 bottom-0 right-0 left-0 object-cover w-full h-full"
+          ></div>
+          <div class="layer"></div>
+        </div>
+        <div class="relative label py-8 px-32 font-semibold bg-secondary">
+          <p class="text-white">Inclusief:</p>
+        </div>
+
+        <div
+          class=" w-full p-8 "
+          v-for="(value, name) in websiteLuxe"
+          :key="value.id"
+        >
+          <div class="relative">
+            <div
+              class="cursor-pointer absolute top-0 bottom-0 right-0 left-0 z-10"
+              @click="e => e.target.parentElement.classList.toggle('active')"
+            ></div>
+            <div
+              class=" flex flex-wrap justify-between relative items-center question px-32"
+            >
+            <v-icon
+            size="30"
+            class="
+          text-main
+          cursor-pointer
+          absolute left-0
+          chevron
+        "
+          >
+            mdi-chevron-right</v-icon
+          >
+              <div >
+                <p class="pointTitle">{{ name }}</p>
+              </div>
+              <v-icon
+                size="30"
+                class="
+              cursor-pointer
+              inline
+              text-success-500
+            "
+              >
+                mdi-check</v-icon
+              >
+            </div>
+            <div class="text-base answer w-full px-32 py-8 text-left">
+              {{ value }}
+            </div>
+          </div>
+        </div>
+        <div class="text-center text-main py-32 ">
+          <p class="font-semibold">35,- Per maand</p>
+          <p class="text-base">Inclusief hosting + domeinnaam</p>
+          <router-link to="/">
+            <div class="ctaButton mt-8 py-8 px-32 inline-block">Bestel</div>
+          </router-link>
+      </div>
+      </div>
+
     </div>
   </div>
 </template>
--- src/components/Prices/price-list.ts
+++ src/components/Prices/price-list.ts
@@ -14,8 +14,21 @@ export default class PriceList extends Vue {
   // }
 
   protected websiteComplete = {
-    "Website design": "Kies je Website Design, ontwikkeld door onze eigen vormgevers. Binnen jouw gekozen Design wordt de Website op maat gemaakt door onze vormgever. We maken eerst een Ontwerpvoorstel zodat we zeker weten dat de Website aan jouw wensen voldoet! ",
-    "Website Pagina's": "Wij ontwikkelen max. 10 pagina’s voor jouw Website. ",
+    "Website design": "Een complete website + ontwerp. De website wordt op maat gemaakt aansluitend aan jouw wensen.",
+    "Website Pagina's": "Tot 10 pagina's inbegrepen.",
+    "Website livezetservice": "Wij helpen je graag je website online te zetten op jouw gekozen domeinnaam.",
+    "Snel": "jouw website is slim ontwikkeld en draait razendsnel.",
+    "Persoonlijk": "Graag gaan we met je in gesprek over je wensen, eventueel kunnen we bij je langskomen.",
+};
+
+  protected websiteLuxe = {
+    "Website design op maat": "Het beste design voor jouw website, ontwikkeld door ervaren UX designers en vormgevers. Ontwikkeld door de beste developers",
+    "Logo, huisstijl, video": "Indien nodig kunnen wij u helpen met fotografie/video, logo's en een huisstijl",
+    "Headless CMS": "Het razendsnelle Vue.JS framework wordt gekoppeld aan een wordpress headless CMS. Hierdoor kan jij eenvoudig je teksten aanpassen terwijl de website zo snel mogelijk is.",
+    "Website Pagina's": "Tot 15 pagina's inbegrepen.",
+    "Website livezetservice": "Wij helpen je graag je website online te zetten op jouw gekozen domeinnaam.",
+    "Snel": "jouw website is slim ontwikkeld en draait razendsnel.",
+    "Persoonlijk": "Graag gaan we met je in gesprek over je wensen, eventueel kunnen we bij je langskomen.",
 };
 
 
