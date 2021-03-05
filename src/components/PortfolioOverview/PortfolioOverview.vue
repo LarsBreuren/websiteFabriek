2396f997e35ff4ff3551e8eecd278ff2e0afef8b
zack siri <larsbreuren@gmail.com>
2021-03-05 10:34:04 +0100

    WIP portfolio

--- src/components/PortfolioOverview/PortfolioOverview.vue
+++ src/components/PortfolioOverview/PortfolioOverview.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="containerFull">  
-         <div class="caseContainer flex" v-for="post in post" :key="post.name">
-          <div class="caseImg relative w-1/2">
-            <img class="absolute h-full w-full object-cover" src="../../assets/images/portfolio/praktijk-thumb.jpg" alt="Portfolio banner background image">
+  <div class="containerFull cases mt-32 sm:mt-0">  
+         <div class="caseContainer flex flex-wrap" v-for="post in post" :key="post.name">
+          <div class="caseImg relative w-full sm:w-1/2">
+            <img class="absolute h-full w-full object-cover" :src="getPostAcfValue(post, 'case_image').url" alt="Portfolio banner background image">
           </div>
-          <div class="caseContentContainer w-1/2 ">
+          <div class="caseContentContainer  w-full sm:w-1/2 ">
             <div class="caseContent relative">
               <p class="bigGreyTitle">
                 {{ getPostAcfValue(post, "grey_title") }}
               </p>
 
             <h2 class="caseHeader">{{ getPostAcfValue(post, "case_title") }}</h2>
-            <p>{{ getPostAcfValue(post, "case_text") }}</p>
-            <router-link to="/portfolio">
+            <p>{{ getPostAcfValue(post, "case_text") }}</p> <!-- ToDo fix <br> / v-html-->
+            <router-link to="/portfolio"> <!-- ToDo fix with acf-->
               <div class="w-1/3 mx-auto ctaButton mt-16">
                 Bekijk case
               </div>
