5328c426513f9ebcf78c4694883188d7f314717d
zack siri <larsbreuren@gmail.com>
2021-03-11 12:34:48 +0100

    Fixed alignment and WIP animation

--- src/components/Proces/Proces.vue
+++ src/components/Proces/Proces.vue
@@ -1,69 +1,39 @@
 <template>
   <div class="container">
-    <h2 class="text-4xl mb-32">Het proces.</h2> 
-    <div class="w-full mx-auto flex items-center">
-      <div class="w-1/3 flex flex-wrap">
-        <div class="bubble bubble1 ml-auto">
-          <div class="w-1/5">
-            <p class="procesStep">1</p>
+    <h2 class="text-4xl mb-32">Het proces.</h2>
+    <p @click="initLoad">Click</p>
+
+    <div class="bubbleContainer">
+      <div class="bubbleGroup">
+        <div v-for="step in steps" :key="step.id">
+          <div v-if="step.show" :class="'bubble' + ( step.number) + ' bubble'">
+            <div class="w-1/5">
+              <p class="procesStep">{{ step.number }}</p>
+            </div>
+            <div class="w-3/5 px-16 ">
+              <h3>{{ step.title }}</h3>
+              <p>{{ step.text }}</p>
+            </div>
+            <div class="w-1/5">
+              <v-icon
+                size="30"
+                class="
+            icon
+          "
+                >mdi-{{ step.icon }}</v-icon
+              >
+            </div>
           </div>
-          <div class="w-3/5 px-16 ">
-            <h3>Vormgevingsgesprek</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem, ipsum dolor.</p>
-          </div>
-          <div class="w-1/5">
-            <v-icon
-              size="30"
-              class="
-              icon
-            "
-              >mdi-notebook-edit-outline</v-icon
-            >
-          </div>
+          
         </div>
-        <div class="bubble bubble2 ml-auto">
-          <div class="w-1/5">
-            <p class="procesStep">2</p>
-          </div>
-          <div class="w-3/5 px-16 ">
-            <h3>1e opzet</h3>
-            <p>Lorem ipsum dolor sit.</p>
-          </div>
-          <div class="w-1/5">
-            <v-icon
-              size="30"
-              class="
-              icon
-            "
-              >mdi-notebook-edit-outline</v-icon
-            >
-          </div>
         </div>
-        <div class="bubble bubble3 ml-auto">
-          <div class="w-1/5">
-            <p class="procesStep">3</p>
-          </div>
-          <div class="w-3/5 px-16 ">
-            <h3>Laatste aanpassingen</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div class="w-1/5">
-            <v-icon
-              size="30"
-              class="
-              icon
-            "
-              >mdi-notebook-edit-outline</v-icon
-            >
-          </div>
-        </div>
-      </div>
-      <div class="px-16">
+
+      <div class="px-16 self-center">
         <div class="">
           <img class="logo " src="../../assets/images/logo-small.svg" />
         </div>
       </div>
-      <div class="w-1/3 flex flex-wrap">
+      <div class="bubbleGroup">
         <div class="bubble bubble4">
           <div class="w-1/5">
             <v-icon
@@ -99,7 +69,6 @@
           <div class="w-1/5">
             <p class="procesStep">5</p>
           </div>
-        
         </div>
         <div class="bubble bubble6">
           <div class="w-1/5">
@@ -118,7 +87,6 @@
           <div class="w-1/5">
             <p class="procesStep">6</p>
           </div>
-        
         </div>
       </div>
     </div>
--- src/components/Proces/proces.ts
+++ src/components/Proces/proces.ts
@@ -4,15 +4,41 @@ import getPostByPostName from "../support/getPostByPostName";
 
 @Component({})
 export default class Headless extends Vue {
-  protected get post(): PostInterface | undefined {
-    return getPostByPostName(this.$store.getters.posts, "herobanner");
+  protected steps = [
+    { number: "1", title: "Vormgevingsgesprek", text: "Lorem ipsum dolor sit amet. Lorem, ipsum dolor.",
+    icon: "notebook-edit-outline", show: false },
+    { number: "2", title: "1e opzet", text: "Lorem ipsum dolor sit amet. Lorem, ipsum dolor.",
+    icon: "notebook-edit-outline", show: false },
+    { number: "3", title: "Aanpassen", text: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.",
+    icon: "notebook-edit-outline", show: false },
+  ];
+  protected steps2 = [
+    { number: "4", title: "Vormgevingsgesprek", text: "Lorem ipsum dolor sit amet. Lorem, ipsum dolor.",
+    icon: "notebook-edit-outline", show: false },
+    { number: "5", title: "1e opzet", text: "Lorem ipsum dolor sit amet. Lorem, ipsum dolor.",
+    icon: "notebook-edit-outline", show: false },
+    { number: "6", title: "Aanpassen", text: "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.",
+    icon: "notebook-edit-outline", show: false },
+  ];
+
+  protected initLoad(){
+    this.steps.forEach((step, idx) => {
+      this.delayLoad(step, idx)
+    })
+    this.initLoad2();
   }
-  protected getPostAcfValue(post: PostInterface | undefined, key: string): any {
-    return post && post.acf && (post.acf as Record<string, any>)[key]
-      ? (post.acf as Record<string, any>)[key]
-      : "Oeps";
+
+  protected initLoad2(){
+    // this.steps2.forEach((step, idx) => {
+    //   this.delayLoad(step, idx)
+    // })
+    console.log('done!')
   }
-  protected get usp(): PostInterface | undefined {
-    return getPostByPostName(this.$store.getters.posts, "usp");
+
+  protected delayLoad(step: any, idx: any){
+      const delay = 1000 * idx
+      setTimeout(() => {
+        step.show = true
+      }, delay)
   }
 }
